ef3f479007eeaf530cb2fd281e2c887e977557d3
Noel Delgado <auhcsei@gmail.com>
2017-08-02 21:46:54 -0500

    create new `lib/index.es.js` file using `format: es`

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,31 @@
 import json from 'rollup-plugin-json';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import buble from 'rollup-plugin-buble';
 import cleanup from 'rollup-plugin-cleanup';
 
-export default {
-  entry: './src/index.js',
-  dest: './lib/index.js',
-  format: 'umd',
-  moduleName: 'DPR',
-  plugins: [
-    json(),
-    nodeResolve({ main: true }),
-    commonjs(),
-    buble(),
-    cleanup()
-  ]
-};
+export default [
+  {
+    entry: './src/index.js',
+    dest: './lib/index.js',
+    format: 'umd',
+    moduleName: 'DPR',
+    plugins: [
+      json(),
+      resolve(),
+      commonjs(),
+      buble(),
+      cleanup()
+    ]
+  },
+  {
+    entry: './src/index.js',
+    dest: './lib/index.es.js',
+    format: 'es',
+    external: [
+      'eventdispatcher.js',
+      'lodash.assign'
+    ],
+    plugins: [ json() ]
+  }
+];
